26c6c73d52c5281ace9158c57e2ec390da0a03c1
RussRussBus <russell@CARLOSs-MacBook-Pro.local>
2025-10-03 11:37:00 -0700

    up

--- russell/03_example/clean.py
+++ russell/03_example/clean.py
@@ -3,5 +3,5 @@ import os
 os.system('rm -r resources/*')
 os.system('rm -r results/*')
 os.system('rm -r workflow/logs/*')
-os.system('rm -r jobs/*')
+# os.system('rm -r jobs/*')
 os.system('ln -s ../../../../datacore2024/genome_celegans/1pct.fa resources/')
