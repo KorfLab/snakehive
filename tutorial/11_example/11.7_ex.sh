b3203804ed15e55bc9e4683f5537dd3e5c8bd6db
RussRussBus <russell.hong@icloud.com>
2026-02-24 19:54:16 -0800

    up

---
+++ tutorial/11_example/11.7_ex.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+declare -a jid
+num_trials=10
+script="run_file.slurm"
+
+for ((num=1; num<=num_trials; num++)); do
+    if [ $num -eq 1 ]; then
+        jid[$num]=$(sbatch --parsable $script)
+    else
+        jid[$num]=$(sbatch --parsable \
+        --dependency=afterany:${jid[$((num-1))]} $script)
+    fi
+done
