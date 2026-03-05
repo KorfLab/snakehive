3bcc0fbbb0f911f02f9eaaf7a10b8f68c0adf759
RussRussBus <russell.hong@icloud.com>
2026-03-04 16:45:45 -0800

    up

--- tutorial/12_example/12.4_ex.sh
+++ tutorial/12_example/12.4_ex.sh
@@ -5,4 +5,4 @@ workflow_run=run_smk.slurm
 
 step_1=$(sbatch --parsable $conda_maker)
 
-sbatch --parsable $workflow_run --dependency=afterany:$(step_1)
+sbatch --parsable $workflow_run --dependency=afterany:$step_1
